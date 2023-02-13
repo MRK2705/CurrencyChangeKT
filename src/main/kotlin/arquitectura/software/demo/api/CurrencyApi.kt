316e989f89f0f4bf558c5293c13c518210faef4b
Mirko Alarcon <mirko.alarcon@ucb.edu.bo>
2023-02-12 23:56:27 -0400

    clase api

---
+++ src/main/kotlin/arquitectura/software/demo/api/CurrencyApi.kt
@@ -0,0 +1,20 @@
+package arquitectura.software.demo.api
+
+import arquitectura.software.demo.dto.CurrencyDto
+import arquitectura.software.demo.dto.ResponseDto
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import arquitectura.software.demo.bl.CurrencyBl
+import java.math.BigDecimal
+
+@RestController
+@RequestMapping("api/v1/currency")
+class CurrencyApi(private val currencyBl: CurrencyBl) {
+
+    @GetMapping("/obtain")
+    fun obtain(@RequestParam from: String, @RequestParam to: String, @RequestParam amount: BigDecimal): ResponseEntity<ResponseDto<Any>> {
+        val currencyDto = currencyBl.obtain(from, to, amount)
+        val responseDto = ResponseDto<Any>(currencyDto, true, "Success")
+        return ResponseEntity.ok(responseDto)
+    }
+}
